cebe93431dbecd334510e324d32e57a45bf5a61c
Luke Newbigging <lnewbigging@lcs.on.ca>
2022-03-02 14:55:06 -0500

    Cleaned up formatting and got my Favourites list to show

--- FurryFriends/Views/DogView.swift
+++ FurryFriends/Views/DogView.swift
@@ -8,33 +8,138 @@
 import SwiftUI
 
 struct DogView: View {
-    // MARK: Stored properties
     
+    // MARK: Stored properties
     // Detect when an app moves between forground, background, and inactive states
     @Environment(\.scenePhase) var scenePhase
     
-    
-    @State var currentFurryFriends: FurryFriends  = FurryFriends(message: "",
-                                                                 status: "")
+    @State var currentDog: Dog = Dog(message: "",
+                                                                status: "")
     
     // This will keep track of our list of favourite jokes
-    @State var favourites: [FurryFriends] = []   // empty list to start
+    @State var favourites: [Dog] = []   // empty list to start
     
     // This will let us know whether the current exists as a favourite
-    @State var currentFurryFriendsAddedToFavourites: Bool = false
+    @State var currentDogAddedToFavourites: Bool = false
     
     // MARK: Computed properties
-    
+    var body: some View {
+        VStack {
+            
+            Text(currentDog.message)
+                .font(.title)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.leading)
+                .padding(30)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 16)
+                        .stroke(Color.primary, lineWidth: 4)
+                )
+                .padding(10)
+            
+            Image(systemName: "heart.circle")
+                .font(.largeTitle)
+            //                      CONDITION                        true   false
+                .foregroundColor(currentDogAddedToFavourites == true ? .red : .secondary)
+                .onTapGesture {
+                    
+                    // Only add to the list if it is not already there
+                    if currentDogAddedToFavourites == false {
+                        
+                        // Adds the current joke to the list
+                        favourites.append(currentDog)
+                        
+                        // Record that we have marked this as a favourite
+                        currentDogAddedToFavourites = true
+                        
+                    }
+                    
+                }
+            
+            Button(action: {
+                
+                // The Task type allows us to run asynchronous code
+                // within a button and have the user interface be updated
+                // when the data is ready.
+                // Since it is asynchronous, other tasks can run while
+                // we wait for the data to come back from the web server.
+                Task {
+                    // Call the function that will get us a new joke!
+                    await loadNewDog()
+                }
+            }, label: {
+                Text("Another one!")
+            })
+                .buttonStyle(.bordered)
+            
+            HStack {
+                Text("Favourites")
+                    .bold()
+                
+                Spacer()
+            }
+            
+            // Iterate over the list of favourites
+            // As we iterate, each individual favourite is
+            // accessible via "currentFavourite"
+            List(favourites, id: \.self) { currentFavourite in
+                Text(currentFavourite.message)
+            }
+            
+            Spacer()
+            
+        }
+        // When the app opens, get a new joke from the web service
+        .task {
+            
+            // Load a joke from the endpoint!
+            // We "calling" or "invoking" the function
+            // named "loadNewJoke"
+            // A term for this is the "call site" of a function
+            // What does "await" mean?
+            // This just means that we, as the programmer, are aware
+            // that this function is asynchronous.
+            // Result might come right away, or, take some time to complete.
+            // ALSO: Any code below this call will run before the function call completes.
+            await loadNewDog()
+            
+            
+            print("I tried to load a new joke")
+            
+            loadFavourites()
+        }
+        //React to changes of state for the app (foreground, background, inactive)
+        .onChange(of: scenePhase) { newPhase in
+            
+            if newPhase == .inactive{
+                print("Inactive")
+            } else if newPhase == .active {
+                print ("Active")
+            } else if newPhase == .background{
+                print ("background")
+                
+                //permantly save the list of tasks
+                persistFavourites()
+                
+                
+            }
+            
+        }
+        .navigationTitle("Furry Friends")
+        .padding()
+      
+               
+    }
     
     // MARK: Functions
     
-    // Define the function "loadFurryFriends"
+    // Define the function "loadNewDog"
     // Teaching our app to do a "new thing"
     //
     // Using the "async" keyword means that this function can potentially
     // be run alongside other tasks that the app needs to do (for example,
     // updating the user interfac
-    func loadNewFurryFriends() async {
+    func loadNewDog() async {
         // Assemble the URL that points to the endpoint
         let url = URL(string: "https://dog.ceo/api/breeds/image/random")!
         
@@ -60,13 +165,13 @@ struct DogView: View {
             //                                 DATA TYPE TO DECODE TO
             //                                         |
             //                                         V
-            currentFurryFriends = try JSONDecoder().decode(FurryFriends.self, from: data)
+            currentDog = try JSONDecoder().decode(Dog.self, from: data)
             
             
             
             // Reset the flag that tracks whether the current joke
             // is a favourite
-            currentFurryFriendsAddedToFavourites = false
+            currentDogAddedToFavourites = false
             
         } catch {
             print("Could not retrieve or decode the JSON from endpoint.")
@@ -132,7 +237,7 @@ struct DogView: View {
             
             //Decode the JSON into Swift native file
             //NOTE: we use [DadJoke] since we are loading into a list (array)
-            favourites = try JSONDecoder().decode([FurryFriends].self, from: data)
+            favourites = try JSONDecoder().decode([Dog].self, from: data)
             
             //Loads the data the was saved to the device
             //Loading our data...
@@ -144,134 +249,6 @@ struct DogView: View {
             print("=======")
             print(error.localizedDescription)
         }
-    }
-    var body: some View {
-        ScrollView{
-            VStack {
-                
-                Text(currentFurryFriends.message)
-                    .font(.title)
-                    .minimumScaleFactor(0.5)
-                    .multilineTextAlignment(.leading)
-                    .padding(30)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 16)
-                            .stroke(Color.primary, lineWidth: 4)
-                    )
-                    .padding(10)
-                
-                Image(systemName: "heart.circle")
-                    .font(.largeTitle)
-                //                      CONDITION                        true   false
-                    .foregroundColor(currentFurryFriendsAddedToFavourites == true ? .red : .secondary)
-                    .onTapGesture {
-                        
-                        // Only add to the list if it is not already there
-                        if currentFurryFriendsAddedToFavourites == false {
-                            
-                            // Adds the current joke to the list
-                            favourites.append(currentFurryFriends)
-                            
-                            // Record that we have marked this as a favourite
-                            currentFurryFriendsAddedToFavourites = true
-                            
-                        }
-                        
-                    }
-                
-                Button(action: {
-                    
-                    // The Task type allows us to run asynchronous code
-                    // within a button and have the user interface be updated
-                    // when the data is ready.
-                    // Since it is asynchronous, other tasks can run while
-                    // we wait for the data to come back from the web server.
-                    Task {
-                        // Call the function that will get us a new joke!
-                        await loadNewFurryFriends()
-                    }
-                }, label: {
-                    Text("Another one!")
-                })
-                    .buttonStyle(.bordered)
-                
-                HStack {
-                    Text("Favourites")
-                        .bold()
-                    
-                    Spacer()
-                }
-                
-                // Iterate over the list of favourites
-                // As we iterate, each individual favourite is
-                // accessible via "currentFavourite"
-                List(favourites, id: \.self) { currentFavourite in
-                    Text(currentFavourite.message)
-                }
-                
-                Spacer()
-                
-            }
-            // When the app opens, get a new joke from the web service
-            .task {
-                
-                // Load a joke from the endpoint!
-                // We "calling" or "invoking" the function
-                // named "loadNewJoke"
-                // A term for this is the "call site" of a function
-                // What does "await" mean?
-                // This just means that we, as the programmer, are aware
-                // that this function is asynchronous.
-                // Result might come right away, or, take some time to complete.
-                // ALSO: Any code below this call will run before the function call completes.
-                await loadNewFurryFriends()
-                
-                
-                print("I tried to load a new joke")
-                
-                loadFavourites()
-            }
-            //React to changes of state for the app (foreground, background, inactive)
-            .onChange(of: scenePhase) { newPhase in
-                
-                if newPhase == .inactive{
-                    print("Inactive")
-                } else if newPhase == .active {
-                    print ("Active")
-                } else if newPhase == .background{
-                    print ("background")
-                    
-                    //permantly save the list of tasks
-                    persistFavourites()
-                    
-                    
-                }
-                
-            }
-            .navigationTitle("Furry Friends")
-            .padding()
-        }
-        
-        // MARK: Functions
-        
-        // Define the function "loadFurryFriends"
-        // Teaching our app to do a "new thing"
-        //
-        // Using the "async" keyword means that this function can potentially
-        // be run alongside other tasks that the app needs to do (for example,
-        // updating the user interface)
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
     }
 }
 
--- FurryFriends/Views/Model /SharedFunctions.swift
+++ FurryFriends/Views/Model /SharedFunctions.swift
@@ -1,18 +1,13 @@
 //
 //  SharedFunctions.swift
-//  FurryFriends
+//  Dog
 //
 //  Created by Luke Newbigging on 2022-03-01.
 //
 
 import Foundation
 
-// The DadJoke structure conforms to the
-// Decodable protocol. This means that we want
-// Swift to be able to take a JSON object
-// and 'decode' into an instance of this
-// structure
-struct FurryFriends: Decodable, Hashable, Encodable {
+struct Dog: Decodable, Hashable, Encodable {
     
     let message: String
     
